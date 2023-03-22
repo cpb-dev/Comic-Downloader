a49dfd92a8fb9142ffd9c4361b3128e53316d112
Conor Bailey <cpbailey025@gmail.com>
2023-03-22 21:08:58 +0000

    Finished for now

--- comic_downloader.py
+++ comic_downloader.py
@@ -6,10 +6,13 @@ from webdriver_auto_update import check_driver
 import urllib.request as req
 import urllib.error
 from os import listdir
-import os
+import os, time
 from glob import glob
 import img2pdf
 import shutil
+import pandas as pd
+
+this_file = os.path.dirname(__file__)
 
 check_driver('/Users/cpbai/AppData/Local/Programs/Python/Python310/Lib/site-packages/selenium/webdriver')
 
@@ -20,27 +23,41 @@ browser = webdriver.Chrome(
     options = options,
 )
 
-def gather_comic():
-    url = "https://readallcomics.com/nightwing-v4-101-2023/"
+def printProgress(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = '\r'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print("\n")
+
+
+def gatherComic(url, comic, issue):
     x_path = "/html/body/div[2]/p/img"
 
     browser.get(url)
 
-    img_dir = "C:/Users/cpbai/PlayGrounds/Comic Downloader/tmp/"
+    img_dir = os.path.join(this_file, "temp")
     images = []
 
     for img in browser.find_elements(by = "xpath", value = x_path):
         image = img.get_attribute('src')
         images.append(image)
     
+    length = len(images) #length of whater the progress bar is for
+    printProgress(0, length, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
     for i in range(len(images)):
         #Create a temporary file for image before pdf
-        file = img_dir + "Nightwing00{}.jpg".format(i)
+        file = os.path.join(img_dir, comic + "{}.jpg".format(i))
         with req.urlopen(images[i]) as d, open(file, "wb") as opfile:
             data = d.read()
             opfile.write(data)
 
-    pdf_path = "C:/Users/cpbai/PlayGrounds/Comic Downloader/Comics/new_comic.pdf"
+        printProgress(i + 1, length, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    pdf_path = os.path.join(this_file, "Comics", comic + " " + issue + ".pdf")
 
     with open(pdf_path, "wb") as f:
         f.write(img2pdf.convert(sorted(glob(os.path.join(img_dir, "*.jpg")), key=len)))
@@ -52,7 +69,12 @@ def gather_comic():
         except OSError:
             os.remove(filepath)
 
-gather_comic()
+    print("Download Complete!")
+    time.sleep(2)
 
-#Control the page to go to next comic, if comic already exists
-#Better organise how the comics are saved and ordered
+if __name__ == "__main__":
+    hero =  input("What hero are you downloading a comic for?\n")
+    issue = input("Issue No. ")
+    url = input("Input URL: ")
+
+    gatherComic(url, hero, issue)
